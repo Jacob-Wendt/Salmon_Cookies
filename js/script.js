ac2ace389af103ab5ecf99c3023cbb01ba071151
Jacob_Wendt <jacob.wendt@yahoo.com>
2019-03-18 15:19:42 -0500

    Added_lit_obj

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+
+var loc1 = {
+minCust: 0,
+maxCust: 0,
+avgSale: 0,
+};
+
+var loc2 = {
+minCust: 0,
+maxCust: 0,
+avgSale: 0
+};
+
+var loc3 = {
+minCust: 0,
+maxCust: 0,
+avgSale: 0
+};
+
+var loc4 = {
+minCust: 0,
+maxCust: 0,
+avgSale: 0
+};
+
+var loc5 = {
+minCust: 0,
+maxCust: 0,
+avgSale: 0
+};
+
