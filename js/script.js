5b85c5918febc99842ae708b28a677c1b465fbed
Jacob_Wendt <jacob.wendt@yahoo.com>
2019-03-18 15:40:04 -0500

    Rnd_Cx_Func

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 
+var hours = ['6am','7am','8am','9am','10am','11am','12pm','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
 
 var loc1 = {
-minCust: 0,
-maxCust: 0,
-avgSale: 0,
+minCust: 23,
+maxCust: 65,
+avgSale: 6.3,
+cx: 0,
+randomCx: function() {
+this.cx = Math.floor(Math.random() * (this.maxCust - this.minCust) ) + this.minCust;
+}
 };
+loc1.randomCx();
+console.log (loc1.cx);
 
 var loc2 = {
 minCust: 0,
