3d1ba727a8a663b582d2ec6d984ea47cd481c547
Jacob_Wendt <jacob.wendt@yahoo.com>
2019-03-19 15:23:04 -0500

    eureka!

--- objects/store.js
+++ objects/store.js
@@ -26,9 +26,25 @@ this.cookiesEachHour.push(Math.ceil(this.customersEachHour[i] * this.avgCookiesP
 Store.prototype.calcTotalCookies = function() {
 for(var i = 0; i < this.cookiesEachHour.length; i++){
 this.totalDailyCookies += this.cookiesEachHour[i];
-}
+};
 };
 
+Store.prototype.renderRow = function() {
+this.calcCustomersEachHour();
+this.calcCookiesEachHour();
+
+var tableRow = document.createElement('tr');
+var location = document.createElement('td');
+location.textContent = this.locationName;
+tableRow.appendChild(location);
+for(var i = 0; i < this.cookiesEachHour.length; i++){
+var tableData = document.createElement('td');
+tableData.textContent = this.cookiesEachHour[i];
+tableRow.appendChild(tableData);
+}
+var renderTable = document.getElementById('renderTable')
+renderTable.appendChild(tableRow);
+};
 
 var pike = new Store('1st and Pike', 23, 65, 6.3);
 var airport = new Store('SeaTac Airport', 3, 24, 1.2);
@@ -36,9 +52,8 @@ var seattle = new Store('Seattle Center', 3, 24, 1.2);
 var capitol = new Store('Capitol Hill', 3, 24, 1.2);
 var alki = new Store('Alki', 3, 24, 1.2);
 
+//var para = document.createElement('p'); //create 
+//para.textContent = "hello world"; //content
+//document.body.appendChild(para); //append
 
-console.log (pike);
-console.log (airport);
-console.log (seattle);
-console.log (capitol);
-console.log (alki);
+pike.renderRow();
